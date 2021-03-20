345b3d145629885e9529b997c2292879a5fa5c72
luanamt <luana.larissam@gmail.com>
2021-03-20 20:18:20 -0300

    First setup test: Serial port

--- Projeto_Microprocessadores/src/main.cpp
+++ Projeto_Microprocessadores/src/main.cpp
@@ -2,8 +2,12 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(250000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Serial connected 250Kbps...");
+
+  delay(500);
 }
